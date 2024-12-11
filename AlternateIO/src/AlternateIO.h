cb193aef5bea5800163bbe555ff3de41dc0cf140
techn1cian <walter.94214@gmail.com>
2024-12-12 09:17:36 +1300

    formatting

--- AlternateIO/src/AlternateIO.h
+++ AlternateIO/src/AlternateIO.h
@@ -21,23 +21,23 @@
 // ----------------------------------------------------------------------------
 // Setup Defines
 
-// #define ALTIO_INVERT_INPUT						- invert inputs globally
-// #define ALTIO_INVERT_OUTPUT						- invert outputs globally
-// #define ALTIO_RETAIN_OUTPUT_STATE				- when writing output store the written state to be retrieved by state() and extending functions
+// #define ALTIO_INVERT_INPUT           // - invert inputs globally
+// #define ALTIO_INVERT_OUTPUT          // - invert outputs globally
+// #define ALTIO_RETAIN_OUTPUT_STATE    // - when writing output store the written state to be retrieved by state() and extending functions
 
-#define ALTIO_DEFAULT_DEBOUNCE	100
-#define ALTIO_DEFAULT_PIN_MODE	INPUT
+#define ALTIO_DEFAULT_DEBOUNCE 100
+#define ALTIO_DEFAULT_PIN_MODE INPUT
 
 #ifndef ALTIO_INVERT_INPUT
 #define _ALTIO_INVERT_INPUT
 #else
-#define _ALTIO_INVERT_INPUT		!
+#define _ALTIO_INVERT_INPUT !
 #endif
 
 #ifndef ALTIO_INVERT_OUTPUT
 #define _ALTIO_INVERT_OUTPUT
 #else
-#define _ALTIO_INVERT_OUTPUT	!
+#define _ALTIO_INVERT_OUTPUT !
 #endif
 
 // ----------------------------------------------------------------------------
@@ -48,19 +48,19 @@
 // Defines
 
 // using ATMega328
-//#define _mcu_					_atmega328_
-#define _atmega328_				_mcu_ // either one of these works
+//#define _mcu_ _atmega328_
+#define _atmega328_ _mcu_ // either one of these works
 
-#define NUMBER_OF_IO_PINS		20
+#define NUMBER_OF_IO_PINS 20
 
-#define FORCE_INLINE			__attribute__((always_inline))
+#define FORCE_INLINE __attribute__((always_inline))
 
-#define STATUS_BIT_PIN			0
-#define STATUS_MSK_PIN			63
-#define STATUS_BIT_STATE		6
-#define STATUS_MSK_STATE		64
-#define STATUS_BIT_LASTSTATE	7
-#define STATUS_MSK_LASTSTATE	128
+#define STATUS_BIT_PIN 0
+#define STATUS_MSK_PIN 63
+#define STATUS_BIT_STATE 6
+#define STATUS_MSK_STATE 64
+#define STATUS_BIT_LASTSTATE 7
+#define STATUS_MSK_LASTSTATE 128
 
 // ----------------------------------------------------------------------------
 
@@ -72,13 +72,13 @@
 class _mcu_
 {
 protected:
-	static inline uint8_t	_read_pin(uint8_t arduino_pin)		FORCE_INLINE;
-	static inline void		_write_pin_lo(uint8_t arduino_pin)	FORCE_INLINE;
-	static inline void		_write_pin_hi(uint8_t arduino_pin)	FORCE_INLINE;
+	static inline uint8_t _read_pin(uint8_t arduino_pin) FORCE_INLINE ;
+	static inline void _write_pin_lo(uint8_t arduino_pin) FORCE_INLINE ;
+	static inline void _write_pin_hi(uint8_t arduino_pin) FORCE_INLINE ;
 public:
-	static uint8_t			read_pin(uint8_t arduino_pin)		;
-	static void				write_pin_lo(uint8_t arduino_pin)	;
-	static void				write_pin_hi(uint8_t arduino_pin)	;
+	static uint8_t read_pin(uint8_t arduino_pin) ;
+	static void write_pin_lo(uint8_t arduino_pin) ;
+	static void write_pin_hi(uint8_t arduino_pin) ;
 };
 
 // ----------------------------------------------------------------------------
@@ -94,34 +94,34 @@ private:
 	// bit0 ... bit5 - pin
 	// bit6 - state
 	// bit7 - last state
-	uint8_t			_status;
+	uint8_t _status;
 
-	inline void		_set_state(uint8_t new_state);
-	uint8_t			_read();
-	void			_lo();
-	void			_hi();
+	inline void _set_state(uint8_t new_state);
+	uint8_t _read();
+	void _lo();
+	void _hi();
 
 public:
-	uint8_t			_state();
-	uint8_t			_last_state();
+	uint8_t _state();
+	uint8_t _last_state();
 
 public:
 	DigitalPin(uint8_t arduino_pin, uint8_t pin_mode = ALTIO_DEFAULT_PIN_MODE);
 
-	void			setup(uint8_t pin_mode);
-	uint8_t			pin();
+	void setup(uint8_t pin_mode);
+	uint8_t pin();
 
-	inline uint8_t	read()					FORCE_INLINE;
-	inline void		lo()					FORCE_INLINE;
-	inline void		hi()					FORCE_INLINE;
-	inline void		write(uint8_t value)	FORCE_INLINE;
+	inline uint8_t read() FORCE_INLINE ;
+	inline void lo() FORCE_INLINE ;
+	inline void hi() FORCE_INLINE ;
+	inline void write(uint8_t value) FORCE_INLINE ;
 
 	// can not be used on io pins operating as outputs, with the execption of state() which
 	// may be used on output pins when ALTIO_RETAIN_OUTPUT_STATE is defined
-	inline uint8_t	state()					FORCE_INLINE;
-	inline uint8_t	last_state()			FORCE_INLINE;
-	bool			change_state()			FORCE_INLINE;
-	void			update()				FORCE_INLINE;
+	inline uint8_t state() FORCE_INLINE ;
+	inline uint8_t last_state() FORCE_INLINE ;
+	bool change_state() FORCE_INLINE ;
+	void update() FORCE_INLINE ;
 };
 
 // ----------------------------------------------------------------------------
@@ -134,25 +134,25 @@ public:
 class Debouncer
 {
 private:
-	DigitalPin*		_digital_io						;
-	uint8_t			_status							;
-	uint16_t		_t_debounce						;
-	uint32_t*		_t_start = 0					;
+	DigitalPin* _digital_io ;
+	uint8_t _status ;
+	uint16_t _t_debounce ;
+	uint32_t* _t_start = 0 ;
 
-	inline void		_toggle_timer()					;
-	uint8_t			_state(uint8_t updates_elapsed)	;
-	uint8_t			_state()						;
-	uint8_t			_last_state()					;
+	inline void _toggle_timer() ;
+	uint8_t _state(uint8_t updates_elapsed) ;
+	uint8_t _state() ;
+	uint8_t _last_state() ;
 
 public:
 	Debouncer(DigitalPin& digital_io, uint16_t debounce_len = ALTIO_DEFAULT_DEBOUNCE);
 
-	void			setup(uint16_t debounce_len)	;
-	inline uint8_t	state(uint8_t updates_elapsed)	FORCE_INLINE;
-	inline uint8_t	state()							FORCE_INLINE;
-	inline uint8_t	last_state()					FORCE_INLINE;
-	bool			change_state()					;
-	void			update()						;
+	void setup(uint16_t debounce_len) ;
+	inline uint8_t  state(uint8_t updates_elapsed)  FORCE_INLINE ;
+	inline uint8_t  state() FORCE_INLINE ;
+	inline uint8_t  last_state() FORCE_INLINE ;
+	bool change_state() ;
+	void update() ;
 };
 
 // ----------------------------------------------------------------------------
@@ -177,7 +177,7 @@ inline void DigitalPin::lo()
 	_hi();
 	#endif
 	#ifdef ALTIO_RETAIN_OUTPUT_STATE
-	_set_state(_ALTIO_INVERT_INPUT false); // read functions only invert input
+	_set_state(_ALTIO_INVERT_INPUT false);  // read functions only invert input
 	#endif
 }
 
